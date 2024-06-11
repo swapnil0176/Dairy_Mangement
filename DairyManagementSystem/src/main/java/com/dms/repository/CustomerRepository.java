673440cfa2d537477773fddfbe9b79a7da1769a8
Swapnil_Tawhare <136835903+swapnil0176@users.noreply.github.com>
2024-06-11 18:30:22 +0530

    Delete DairyManagementSystem directory

--- DairyManagementSystem/src/main/java/com/dms/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dms.entities.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer>{
-
-}
--- DairyManagementSystem/src/main/java/com/dms/repository/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-	package com.dms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dms.entities.Supplier;
-
-public interface SupplierRepository extends JpaRepository<Supplier, Integer>{
-
-}
